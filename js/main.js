b7c1f0b5e7c0c755b8febc9fb696aea5dc34d4c5
Dmitry <prokopenkov.dmi@gmail.com>
2020-02-06 03:48:41 +0300

    Add function mockAdsData

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-var NUMBER_OF_ADS = [];
-NUMBER_OF_ADS.length = 8;
-var TYPE_OF_HOUSING = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
-var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
-var FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS_LIST = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
 var pinTemplateElement = document.querySelector('#pin').content.querySelector('.map__pin');
 
 var getRandomInteger = function (min, max) {
@@ -18,9 +10,18 @@ var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 var mapPinsElement = map.querySelector('.map__pins');
 
-var renderAds = function (ads) {
-  for (var i = 0; i < ads.length; i++) {
-    ads[i] = {
+var mockAdsData = function (arrayLength) {
+  var array = [];
+  array.length = arrayLength;
+
+  var typeOfHousing = ['palace', 'flat', 'house', 'bungalo'];
+  var checkinTime = ['12:00', '13:00', '14:00'];
+  var checkoutTime = ['12:00', '13:00', '14:00'];
+  var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var photosList = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+  for (var i = 0; i < arrayLength; i++) {
+    array[i] = {
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png',
       },
@@ -32,34 +33,39 @@ var renderAds = function (ads) {
         title: 'Заголовок предложения',
         address: '600, 350',
         price: (getRandomInteger(0, 100) * 100),
-        type: TYPE_OF_HOUSING[getRandomInteger(0, 4)],
+        type: typeOfHousing[getRandomInteger(0, 4)],
         rooms: getRandomInteger(1, 3),
         guests: getRandomInteger(0, 3),
-        checkin: CHECKIN_TIME[getRandomInteger(0, CHECKIN_TIME.length - 1)],
-        checkout: CHECKOUT_TIME[getRandomInteger(0, CHECKOUT_TIME.length - 1)],
-        features: FEATURES_LIST[getRandomInteger(0, FEATURES_LIST.length - 1)],
+        checkin: checkinTime[getRandomInteger(0, checkinTime.length - 1)],
+        checkout: checkoutTime[getRandomInteger(0, checkoutTime.length - 1)],
+        features: featuresList[getRandomInteger(0, featuresList.length - 1)],
         description: 'Это описание',
-        photos: PHOTOS_LIST = [getRandomInteger(0, PHOTOS_LIST.length - 1)]
+        photos: photosList = [getRandomInteger(0, photosList.length - 1)]
       }
     };
-    var renderPin = function (pin) {
-      var pinElement = pinTemplateElement.cloneNode(true);
-      var renderPointCoordinateX = (pin.location.x - 25);
-      var renderPointCoordinateY = (pin.location.y - 65);
-      pinElement.style = 'left: ' + renderPointCoordinateX + 'px; ' + 'top: ' + renderPointCoordinateY + 'px;';
-      var pinImage = pinElement.querySelector('img');
-      pinImage.src = pin.author.avatar;
-      pinImage.alt = pin.offer.title;
-
-      return pinElement;
-    };
-
   }
+  return array;
+};
+
+var renderPin = function (pin) {
+  var pinElement = pinTemplateElement.cloneNode(true);
+  var renderPointCoordinateX = (pin.location.x - 25);
+  var renderPointCoordinateY = (pin.location.y - 65);
+  pinElement.style = 'left: ' + renderPointCoordinateX + 'px; ' + 'top: ' + renderPointCoordinateY + 'px;';
+  var pinImage = pinElement.querySelector('img');
+  pinImage.src = pin.author.avatar;
+  pinImage.alt = pin.offer.title;
+
+  return pinElement;
+};
+
+var renderAds = function (numberOfAds) {
+  var array = mockAdsData(numberOfAds);
   var fragment = document.createDocumentFragment();
-  for (i = 0; i < ads.length; i++) {
-    fragment.appendChild(renderPin(ads[i]));
+  for (var i = 0; i < array.length; i++) {
+    fragment.appendChild(renderPin(array[i]));
   }
   mapPinsElement.appendChild(fragment);
 };
 
-renderAds(NUMBER_OF_ADS);
+renderAds(8);
