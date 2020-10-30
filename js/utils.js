97d6bd1d07e1148bd6b498d8e462a0e9fac5f3bf
unknown <prokopenkov.dmi@gmail.com>
2020-10-30 22:11:02 +0300

    Add cards rendering for pins

--- js/utils.js
+++ js/utils.js
@@ -4,8 +4,17 @@
   var getRandomInteger = function (min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   };
+
+  var getNewRandomArray = function (arr) {
+    var shuffled = arr.sort(function () {
+      return (0.5 - Math.random());
+    });
+    return shuffled.slice(0, getRandomInteger(0, (arr.length - 1)));
+  };
+
   window.utils = {
-    getRandomInteger: getRandomInteger
+    getRandomInteger: getRandomInteger,
+    getNewRandomArray: getNewRandomArray,
   };
 })();
 
